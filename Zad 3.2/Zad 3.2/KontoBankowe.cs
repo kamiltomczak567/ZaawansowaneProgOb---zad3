f0b37c0a40e0f4d716c5052b08f0b53a6d95b782
kamiltomczak567 <kamil.tomczak567@googlemail.com>
2017-05-09 15:28:01 +0200

    commit; git commit -m commit
    $git commit -m commit
    ;

---
+++ Zad 3.2/Zad 3.2/KontoBankowe.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_3._2
+{
+    public class KontoBankowe
+    {
+        public string Imie;
+        public string Nazwisko;
+        public string NrKonta;
+        public double Fundusze;
+        private int id;
+        public string status;
+
+        static int nr = 1;
+
+        public KontoBankowe(string Imie, string Nazwisko, string NrKonta, double Fundusze)
+        {
+            this.Imie = Imie;
+            this.Nazwisko = Nazwisko;
+            this.NrKonta = NrKonta;
+            this.Fundusze = Fundusze;
+            this.id = nr;
+            this.status = "Aktywny";
+            nr++;
+        }
+
+        public void WplacPieniadze(double kwota)
+        {
+            ZmienFunduszeKonta(this.Fundusze + kwota);
+        }
+
+        private void ZmienFunduszeKonta(double fundusze)
+        {
+            this.Fundusze = fundusze;
+        }
+
+        public double AktualnyStanKonta()
+        {
+            return this.Fundusze;
+        }
+        public void WyplacPieniadze(double kwota)
+        {
+            if (kwota <= this.Fundusze)
+                ZmienFunduszeKonta(this.Fundusze - kwota);
+        }
+        public void setStatus(string status)
+        {
+            this.status = status;
+        }
+        public void TransferujKwote(double kwota, KontoBankowe konto)
+        {
+            this.Fundusze -= kwota;
+            konto.WplacPieniadze(kwota);
+        }
+
+    }
+}
